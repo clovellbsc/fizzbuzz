f1585baf6fd9e1fa3a3af22d6ae0c86a10e53ecc
Chris <clovellbsc@gmail.com>
2022-02-03 11:20:17 +0000

    Complete fizzbuzz and random testing

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,11 @@
 def fizzbuzz(number)
-  if number == 3
-    return 'fizz'
-  elsif number == 5
-    return 'buzz'
-  else
+  if number % 3 == 0 && number % 5 == 0
     return 'fizzbuzz'
+  elsif number % 5 == 0
+    return 'buzz'
+  elsif number % 3 == 0
+    return 'fizz'
+  else 
+    return number
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,4 +10,19 @@ describe "fizzbuzz" do
   it "returns 'fizzbuzz' when passed 15" do
     expect(fizzbuzz(15)).to eq "fizzbuzz"
   end
+  it "returns '4' when passed 4" do
+    expect(fizzbuzz(4)).to eq 4
+  end
+  it "returns 'fizz' when passed 9" do
+    expect(fizzbuzz(9)).to eq "fizz"
+  end
+  it "returns 'fizz' when passed 12" do
+    expect(fizzbuzz(12)).to eq "fizz"
+  end
+  it "returns 'buzz' when passed 20" do
+    expect(fizzbuzz(20)).to eq "buzz"
+  end
+  it "returns 17 when passed 17" do
+    expect(fizzbuzz(17)).to eq 17
+  end
 end
